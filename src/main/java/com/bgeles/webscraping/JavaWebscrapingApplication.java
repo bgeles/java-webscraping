e98caefd79370048ee7db524bd0550414120d78f
Bruno Glauco Eles <bgeles@gmail.com>
2025-06-01 13:35:14 -0300

    initial setup

---
+++ src/main/java/com/bgeles/webscraping/JavaWebscrapingApplication.java
@@ -0,0 +1,13 @@
+package com.bgeles.webscraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebscrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebscrapingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgeles/webscraping/JavaWebscrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgeles.webscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebscrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
